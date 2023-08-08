732fe23ffe23db448942b4dca8e0f478f3980c07
rakibhossain431 <rakibjp431@gmail.com>
2023-08-08 19:08:37 +0600

    Add New Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function($){
     $('.team-slider').slick({
         dots: false,
@@ -88,6 +89,7 @@ $(document).ready(function($){
 
 // Card Price Toggle
 
+
 const checkbox = document.getElementById("pills-tab");
 const starter = document.getElementById("starter");
 const agency = document.getElementById("agency");
@@ -97,14 +99,160 @@ const professionalText = document.getElementById("professionalText");
 const agencyText = document.getElementById("agencyText");
 const starterText = document.getElementById("starterText");
 
-checkbox.addEventListener("click", () => {
-    professional.textContent = professional.textContent === "199" ? "69" : "199";
-    starter.textContent = starter.textContent === "250" ? "99 " : "250";
-    agency.textContent = agency.textContent === "549" ? "199" : "549";
-
-    professionalText.textContent = professionalText.textContent === "ye" ? "mo" : "ye";
-    agencyText.textContent = agencyText.textContent === "ye" ? "mo" : "ye";
-    starterText.textContent = starterText.textContent === "ye" ? "mo" : "ye";
+window.addEventListener("load", (event) => {
+    checkbox.addEventListener("click", () => {
+        professional.textContent = professional.textContent === "199" ? "69" : "199";
+        starter.textContent = starter.textContent === "250" ? "99 " : "250";
+        agency.textContent = agency.textContent === "549" ? "199" : "549";
+        
+        professionalText.textContent = professionalText.textContent === "y" ? "m" : "y";
+        agencyText.textContent = agencyText.textContent === "y" ? "m" : "y";
+        starterText.textContent = starterText.textContent === "y" ? "m" : "y";
+    });
   });
+
   
+
+//  All Pagination Start Hear
+const paginationNumbers = document.getElementById("pagination-numbers");
+const paginatedList = document.getElementById("paginated-list");
+const listItems = paginatedList.querySelectorAll(".paginated-box");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
+
+const paginationLimit = 6;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    button.classList.remove("active");
+    const pageIndex = Number(button.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      button.classList.add("active");
+    }
+  });
+};
+
+const appendPageNumber = (index) => {
+  const pageNumber = document.createElement("button");
+  pageNumber.className = "pagination-number";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + index);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const setCurrentPage = (pageNum) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+  handlePageButtonsStatus();
+  
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {
+    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hidden");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
+
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    const pageIndex = Number(button.getAttribute("page-index"));
+
+    if (pageIndex) {
+      button.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
+
+// Pagination End Hear
+
+// Gellary Poupup
+        
+popup = {
+    init: function(){
+      $('figure').click(function(){
+        popup.open($(this));
+      });
+      
+      $(document).on('click', '.popup img', function(){
+        return false;
+      }).on('click', '.popup', function(){
+        popup.close();
+      })
+    },
+open: function($figure) {
+  $('.gallery').addClass('pop');
+  $popup = $('<div class="popup" />').appendTo($('body'));
+  $fig = $figure.clone().appendTo($('.popup'));
+  $bg = $('<div class="bg" />').appendTo($('.popup'));
+  $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>').appendTo($fig);
+  $shadow = $('<div class="shadow" />').appendTo($fig);
+  src = $('img', $fig).attr('src');
+  $shadow.css({backgroundImage: 'url(' + src + ')'});
+  $bg.css({backgroundImage: 'url(' + src + ')'});
+  setTimeout(function(){
+    $('.popup').addClass('pop');
+  }, 10);
+},
+close: function(){
+  $('.gallery, .popup').removeClass('pop');
+  setTimeout(function(){
+    $('.popup').remove()
+  }, 100);
+}
+}
+
+popup.init()
+
+
 
